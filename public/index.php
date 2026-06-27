7df05269f980c8a64d55d1d921b12932268c183f
Osvaldo Nanastonis <obninformatica@gmail.com>
2026-06-27 01:45:20 -0300

    Criando os arquivos
    criando arquivos para o sistema

---
+++ public/index.php
@@ -0,0 +1,21 @@
+<?php
+//print_r($_SERVER["REQUEST_URI"]);
+if(preg_match('/\.(?:png|jpg|jpeg|gif)$/', $_SERVER["REQUEST_URI"]))
+{
+	return false;	// serve the request resources as-is 
+}
+else
+{
+	//echo "<p>Welcome to PHP</p>";
+
+	session_start();
+
+
+	define('DEFAULT_CONTROLLER','home');
+	define('DEFAULT_METHOD','index');
+
+
+	require '../vendor/autoload.php';
+	require '../App/Functions/functions_twig.php';
+	require 'bootstrap/bootstrap.php';
+}
